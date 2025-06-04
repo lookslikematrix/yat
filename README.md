a5ab7cd3a3125b4a984a246f6dfcdbeba20d1caf
Christian Decker <christian.decker@lookslikematrix.de>
2025-06-04 09:18:30 +0200

    Fixed copy & paste error.

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # 🔥 YaT - Yet another Template
-
-
--- stages/pre-commit/run.sh
+++ stages/pre-commit/run.sh
@@ -3,4 +3,4 @@ set -e
 
 CHANGED_FILES=$(git diff --name-only ${TARGET_BRANCH:-origin/main}...HEAD)
 echo "[ $CHANGED_FILES | main ] 🔎 Changed files:"
-/.yat/venv/bin/pre-commit run --files \$CHANGED_FILES
+/.yat/venv/bin/pre-commit run --files $CHANGED_FILES
