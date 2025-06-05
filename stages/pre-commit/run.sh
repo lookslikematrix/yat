89c26f0219f8f2e1062ab9ca9f6ae64510d9f376
Christian Decker <christian.decker@lookslikematrix.de>
2025-06-05 16:48:02 +0200

    Print $TARGET_BRANCH in run.sh.

--- stages/pre-commit/run.sh
+++ stages/pre-commit/run.sh
@@ -2,5 +2,5 @@
 set -e
 
 CHANGED_FILES=$(git diff --name-only ${TARGET_BRANCH:-origin/main}...HEAD)
-echo "[ $CHANGED_FILES | main ] 🔎 Changed files:"
+echo "[ $CHANGED_FILES | $TARGET_BRANCH] 🔎 Changed files related to target branch:"
 /.yat/venv/bin/pre-commit run --files $CHANGED_FILES
