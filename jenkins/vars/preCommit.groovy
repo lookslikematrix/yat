f5d127e744d8a60464bcdb5704aeef5c581173d6
Christian Decker <christian.decker@lookslikematrix.de>
2025-06-05 11:42:50 +0200

    Add pipeline.

--- jenkins/vars/preCommit.groovy
+++ jenkins/vars/preCommit.groovy
@@ -1,17 +1,8 @@
 def call(Map config = [:]) {
+    String target_branch = config.get('target_branch', 'origin/main')
     stage("🔶 pre-commit") {
-        scriptDir = new File(getClass().protectionDomain.codeSource.location.path).parent
-        echo scriptDir
-
-        docker.image("python:latest").inside {
-            sh """#!/bin/bash
-                set -e
-                echo "🔶 pre-commit"
-                echo "The script you are running has:"
-                echo "basename: [\$(basename "\$0")]"
-                echo "dirname : [\$(dirname "\$0")]"
-                echo "pwd     : [\$(pwd)]"
-            """
+        docker.image("lookslikematrix/yat-pre-commit:latest").inside("--env TARGET_BRANCH=${target_branch}"){
+            sh '/bin/sh /.yat/run.sh'
         }
     }
 }
